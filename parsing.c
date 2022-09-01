da7873e5e8422920b525808062dbd08a6884b492
EbnerLautaro <lautiebner@gmail.com>
2022-09-01 17:05:46 -0300

    parse_scommand() enhanced

--- parsing.c
+++ parsing.c
@@ -4,7 +4,19 @@
 #include "parser.h"
 #include "parsing.h"
 
-static scommand parse_scommand(Parser p) {
+
+
+/**
+ * Devuelve un comando ya parseado, Si se encuentra un símbolo del
+ * operador pipe (|) o un fin de línea ("\n"), el procesamiento no avanza 
+ * y retorna lo parseado hasta ese punto.
+ * 
+ * Si solo encuentra un "\n" o hay error de parseo devuelve NULL.
+ * 
+ * 'is_background' indica si se encontro un ampersand (&), lo consume
+ *  y retorna lo parseado hasta ese punto. 
+ */
+static scommand parse_scommand(Parser p, bool* is_background) {
     /* Devuelve NULL cuando hay un error de parseo */
     assert(p!=NULL);
     if (!parser_at_eof(p)) {
@@ -14,18 +26,22 @@ static scommand parse_scommand(Parser p) {
         do {
             parser_skip_blanks(p);
             data = parser_next_argument(p, &type);
-            switch (* type) {
-            case ARG_NORMAL:
-                scommand_push_back(cmd, data);
-                break;
-            case ARG_INPUT:
-                scommand_set_redir_in(cmd, data);
-                break;
-            case ARG_OUTPUT:
-                scommand_set_redir_out(cmd, data);
-                break;
+            if (*data = '&') {
+                *is_background = true;
+            } else {
+                switch (* type) {
+                case ARG_NORMAL:
+                    scommand_push_back(cmd, data);
+                    break;
+                case ARG_INPUT:
+                    scommand_set_redir_in(cmd, data);
+                    break;
+                case ARG_OUTPUT:
+                    scommand_set_redir_out(cmd, data);
+                    break;
+                }
             }
-        } while (data != NULL);
+        } while (data != NULL && !(*is_background));
         return cmd;
     } else {
         return NULL;
