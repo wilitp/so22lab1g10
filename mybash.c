b1a5727af367a0f2eec365cf2345a1949b75f747
wilitp <guilledeipola@gmail.com>
2022-09-03 03:00:13 -0300

    parse_pipeline

--- mybash.c
+++ mybash.c
@@ -1,36 +1,42 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
+#include "builtin.h"
 #include "command.h"
 #include "execute.h"
 #include "parser.h"
 #include "parsing.h"
-#include "builtin.h"
 
 static void show_prompt(void) {
-    printf ("mybash> ");
-    fflush (stdout);
+    printf("mybash> ");
+    fflush(stdout);
 }
 
 int main(int argc, char *argv[]) {
-    // pipeline pipe;
+    pipeline pipe;
     Parser input;
     bool quit = false;
 
     input = parser_new(stdin);
     while (!quit) {
         show_prompt();
-        // pipe = parse_pipeline(input);
+        pipe = parse_pipeline(input);
+        // printf("%p", (void *)pipe);
 
         /* Hay que salir luego de ejecutar? */
         quit = parser_at_eof(input);
+
+        char *pstr = pipeline_to_string(pipe);
+        printf("%s\n", pstr);
+        free(pstr);
+
         /*
          * COMPLETAR
          *
          */
     }
-    parser_destroy(input); input = NULL;
+    parser_destroy(input);
+    input = NULL;
     return EXIT_SUCCESS;
 }
-
--- parsing.c
+++ parsing.c
@@ -1,52 +1,53 @@
-#include <stdbool.h>
-#include <stdlib.h>
-#include <assert.h>
+#include "parsing.h"
 #include "command.h"
 #include "parser.h"
-#include "parsing.h"
-
-
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 /**
  * Devuelve un comando ya parseado, Si se encuentra un símbolo del
- * operador pipe (|) o un fin de línea ("\n"), el procesamiento no avanza 
+ * operador pipe (|) o un fin de línea ("\n"), el procesamiento no avanza
  * y retorna lo parseado hasta ese punto.
- * 
+ *
  * Si solo encuentra un "\n" o hay error de parseo devuelve NULL.
- * 
+ *
  * 'is_background' indica si se encontro un ampersand (&), lo consume
- *  y retorna lo parseado hasta ese punto. 
+ *  y retorna lo parseado hasta ese punto.
  */
-static scommand parse_scommand(Parser parser, bool* is_background) {
+static scommand parse_scommand(Parser parser, bool *is_background) {
     /* Devuelve NULL cuando hay un error de parseo */
-    assert(parser!=NULL);
+    assert(parser != NULL);
     if (parser_at_eof(parser)) {
         return NULL;
     }
     scommand cmd = scommand_new();
     arg_kind_t type;
     char *data = NULL;
-    do { 
+    do {
         parser_skip_blanks(parser); // consume todos los espacios (" ")
         if (parser_at_eof(parser)) {
             break;
         }
         parser_op_background(parser, is_background);
-        if (!(* is_background) && !parser_at_eof(parser)) {
-            data = parser_next_argument(parser, &type); // consume todo hasta el proximo espacio (" ")
-            switch (type) {
-            case ARG_NORMAL:
-                scommand_push_back(cmd, data);
-                break;
-            case ARG_INPUT:
-                scommand_set_redir_in(cmd, data);
-                break;
-            case ARG_OUTPUT:
-                scommand_set_redir_out(cmd, data);
-                break;
+        if (!(*is_background) && !parser_at_eof(parser)) {
+            data = parser_next_argument(
+                parser, &type); // consume todo hasta el proximo espacio (" ")
+            if (data != NULL) {
+                switch (type) {
+                case ARG_NORMAL:
+                    scommand_push_back(cmd, data);
+                    break;
+                case ARG_INPUT:
+                    scommand_set_redir_in(cmd, data);
+                    break;
+                case ARG_OUTPUT:
+                    scommand_set_redir_out(cmd, data);
+                    break;
+                }
             }
         }
-    } while ((data!=NULL) && !(*is_background) && !parser_at_eof(parser));
+    } while ((data != NULL) && !(*is_background) && !parser_at_eof(parser));
     return cmd;
 }
 
@@ -54,25 +55,38 @@ pipeline parse_pipeline(Parser parser) {
 
     pipeline result = pipeline_new();
     bool is_background = false;
-    scommand cmd = parse_scommand(parser, &is_background);
-    bool error = (cmd==NULL);
-    bool another_pipe = true;
+    bool another_pipe;
+    bool error;
 
-    while (another_pipe && !error && !is_background) {
-        pipeline_push_back(result, cmd);
-        parser_skip_blanks(parser);
-        parser_op_pipe(parser, &another_pipe);
-        if (another_pipe) {
-            cmd = parse_scommand(parser, &is_background);
+    do {
+        // Leemos un comando
+        scommand cmd = parse_scommand(parser, &is_background);
+        error = (cmd == NULL);
+        if(error){
+            break;
         }
-        error = (cmd==NULL);
-    }
-    pipeline_set_wait(result, is_background);
 
-    /* Tolerancia a espacios posteriores */
-    /* Consumir todo lo que hay inclusive el \n */
-    /* Si hubo error, hacemos cleanup */
-    return result; 
+        if (scommand_length(cmd) > 0) {
+            // Si el comando no es vacio lo agregamos
+            pipeline_push_back(result, cmd);
+            parser_skip_blanks(parser);
+            parser_op_pipe(parser,
+                           &another_pipe); // Vemos si hay otro comando pipeado
+        } else {
+            another_pipe = true; // para que vuelva a leer un comando
+
+            // Si el comando es vacio probablemente estemos trabados en un \n.
+            // Lo consumimos.
+            bool garbage = false;
+            parser_garbage(parser, &garbage);
+            if (garbage) {
+                printf("wtf la garbage era: %s\n", parser_last_garbage(parser));
+                exit(1);
+            }
+        }
+    } while (another_pipe && !is_background);
+
+    pipeline_set_wait(result, !is_background);
+
+    return result;
 }
-
-
