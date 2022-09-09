f5f3e5dd3b38807a58cfde47bca68f257c397fb0
wilitp <guilledeipola@gmail.com>
2022-09-09 00:12:04 -0300

    Redireccion de input y output para un solo comando

--- execute.c
+++ execute.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,8 +32,38 @@ static char **arg_array(scommand cmd, unsigned int argc) {
  * Tambien encarga de cerrar la salida del pipe abierto por el comando anterior.
  * SOLO DEBE LLAMARSE DESDE UN PROCESO HIJO
  */
-// static void in_out_redirs(scommand cmd, unsigned long *pipe_prev,
-//                           unsigned long *pipe_next) {}
+static void in_out_redirs(scommand cmd, unsigned long *pipe_prev,
+                          unsigned long *pipe_next) {
+
+    // Variable auxiliar para los nombres de los archivos
+    char *fname_aux = NULL;
+
+    int in, out;
+
+    if ((fname_aux = scommand_get_redir_in(cmd))) {
+        // Abrir archivo y asignar in
+        in = open(fname_aux, O_RDONLY);
+        if (in < 0) {
+            printf("File %s not found.\n", fname_aux);
+        }
+        else {
+            printf("File desc: %d", in);
+            dup2(in, STDIN_FILENO);
+        }
+    } else if (pipe_prev) {
+        in = (int)*pipe_prev;
+        dup2(in, STDIN_FILENO);
+    }
+
+    if ((fname_aux = scommand_get_redir_out(cmd))) {
+        // Abrir archivo y asignar in
+        out = open(fname_aux, O_WRONLY | O_CREAT, 0666);
+        dup2(out, STDOUT_FILENO);
+    } else if (pipe_next) {
+        out = (int)*pipe_next;
+        dup2(out, STDOUT_FILENO);
+    }
+}
 
 void execute_pipeline(pipeline pipe) {
     unsigned int plen = pipeline_length(pipe);
@@ -46,14 +77,15 @@ void execute_pipeline(pipeline pipe) {
 
         // Hijo
         if (0 == (cpid = fork())) {
-
+            in_out_redirs(cmd, NULL, NULL);
             if (strcmp(argv[0], "cd") == 0) { // No anda esto
                 chdir(argv[1]);
-                argv = free_arg_array(argv, argc);
                 exit(0);
             } else if (execvp(argv[0], argv) < 0) {
+                argv = free_arg_array(argv, argc);
                 exit(1);
             }
+            argv = free_arg_array(argv, argc);
 
             // Padre
         } else {
